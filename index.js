1a967a36f727aaa116654b6502d79551bb5dacb0
Evelyn Campos <elenuco36@gmail.com>
2020-09-28 14:49:00 -0600

    cambios
    cambios en el index.js, index.php  y el modelo no muestra nada aun.

--- index.js
+++ index.js
@@ -2,49 +2,25 @@ function init(){
 
 }
 $(document).ready(function(){
-    $('#pokemon_table').dataTable({
-        "aProcesing":true,//activamos el procesamiento de dataTables
-        "aServerSide":true,//Paginacion y filtrados realizados por el servidor
-        "searching":true,
-        lengthChange:true,
-        colReorder:true,
-        "ajax":{
-            url:'controller/pokemon.php?op=listar',
-            type:"post",
-            dataType:"json",
-            error: function(e){
-                console.log(e.resposeText);
-            }
-        },
-        "bDestroy":true,
-        "responsive":true,
-        "bInfo":true,
-        "iDisplayLength":10,
-        "order":[[1,"asc"]],
-        "language":{
-            "sProcesing": "Procesando.....",
-            "sLengthMenu": "Mostrar _MENU_registros",
-            "sZeroRecords":"No se en contraron resultados",
-            "sEmptyTable": " Ningun dato disponible en esta tabla",
-            "sInfo" : "Mostrando un total de _TOTAL_ registros",
-            "sInfoEmpty":"Mostrando un total de 0 registros",
-            "sInfoFiltered": "(filtrando un total de _MAX_ registros)",
-            "sInfoPostfix": "",
-            "sSearch" : "Buscar:",
-            "sUrl": "",
-            "sInfoThousands": ",",
-            "sLoadingRecords": "Cargando...",
-            "oPaginate": {
-                "sFirst": "Primero",
-                "sLast" : "Ultimo",
-                "sNext" : "Siguente",
-                "sPrevious": "Anterior"
-            },
-            "oAria": {
-                "aSortAscending": "Activar para ordenar la columna de manera ascendente",
-                "aSortDescending":"Activa para ordenar la columna de manera descendente"
-            }
-        }
-    }).DataTable();
-});
+	$('#pokemon_tabla').DataTable( {
+		"ajax": {
+			            "type": "GET",
+			            "url": "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=5&fbclid=IwAR1Zvu99a0yvqm52sRC994gyUm0d-t-Tiir5PI-IY7O0lkInPcbAiWnnmWc",
+			            "dataSrc": "",
+			            "beforeSend": function(request) {
+			            },
+			            "data": function(d) {
+			            },
+			        },
+			        "lengthMenu": [
+			            [5, 10, 12, -1],
+			            [5, 10, 12, "All"]
+			        ],
+			        "pageLength": 10,
+			        "cache": false,
+			        "order": [
+			            [0, 'desc']
+			        ],
+	} ).DataTable();
+} );
 init();
--- models/Pokemon.php
+++ models/Pokemon.php
@@ -1,16 +1,15 @@
 <?php
 class Pokemon{
     public function get_pokemon(){
-     $url='https://pokeapi.co/api/v2/pokemon/?offset=0&limit=5';
+        $url='httpss://pokeapi.co/api/v2/pokemon/?offset=0&limit=5';
      $json= file_get_contents($url);
      $array=json_decode($json,true);
      return $array;
     }
-    public function get_pokemon_x_id(){
-        $url='https://pokeapi.co/api/v2/pokemon/?offset=0&limit=5';
+    public function get_pokemon_x_id($url){
         $json= file_get_contents($url);
         $array=json_decode($json,true);
-        return $array;
+        return $array['sprites']["frontdefault"];
        }
 }
 ?>
