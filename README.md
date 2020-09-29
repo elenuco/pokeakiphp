14d7367937cf6be1f9cd5a8a5b4539317d40113e
Evelyn Campos <elenuco36@gmail.com>
2020-09-28 21:10:24 -0600

    ultimos cambios para testear.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pokeakiphp
 Prueba laboral para happy punk panda.
+El desarrollo se ha hecho de manera local por lo tanto es importante usar xammp  o wammp.
+Estar conectado a git.
+
+
+
+
+
+
+
+
+
--- models/Pokemon.php
+++ models/Pokemon.php
@@ -1,7 +1,7 @@
 <?php
 class Pokemon{
     public function get_pokemon(){
-        $url='httpss://pokeapi.co/api/v2/pokemon/?offset=0&limit=5';
+        $url='https://pokeapi.co/api/v2/pokemon/?offset=0&limit=5';
      $json= file_get_contents($url);
      $array=json_decode($json,true);
      return $array;
