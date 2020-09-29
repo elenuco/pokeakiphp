d34dbd2b34ea5e4536a3f4acb6b15e93e9a97fed
Evelyn Campos <elenuco36@gmail.com>
2020-09-28 23:08:39 -0600

    cambio de 2 pokemon a 150.
    cambio en el modelo.

--- models/Pokemon.php
+++ models/Pokemon.php
@@ -1,7 +1,7 @@
 <?php
 class Pokemon{
     public function get_pokemon(){
-        $url='https://pokeapi.co/api/v2/pokemon/?offset=0&limit=2';
+        $url='https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150';
         $json= file_get_contents($url);
         $array=json_decode($json,true);
         return $array;
